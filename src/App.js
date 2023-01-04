2353e78b1ce0466fc3f91e3d8ab1b77678ed3153
Safiriaa <101689115+Safiriaa@users.noreply.github.com>
2023-01-03 21:40:31 -0800

    init

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <section className="red-container">
+        <ul id="party">
+          <li className="pokemon-list">+</li>
+          <li className="pokemon-list">+</li>
+          <li className="pokemon-list">+</li>
+          <li className="pokemon-list">+</li>
+          <li className="pokemon-list">+</li>
+          <li className="pokemon-list">+</li>
+          <li className="pokemon-list">+</li>
+        </ul>
+      </section>
+      <section className="blue-container">
+        <p>Image goes here</p>
+      </section>
     </div>
   );
 }
