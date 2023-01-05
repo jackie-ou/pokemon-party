b17bd020584442bc07c8aa8be815849fc27b1899
jackie-ou <jhgou@ucdavis.edu>
2023-01-05 04:29:14 -0800

    fetched json from api

--- src/Load.js
+++ src/Load.js
@@ -1,9 +1,17 @@
-function Load() {
-    function loadAPI(){
-        // Load API into JSON
-
+const loadAPI = async () => {
+    try{
+        // Fetch API which returns [name : url]
+        let response = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=2');
+        let data = response.json().then((obj) => {
+            console.log(obj.results);
+            return obj.results;
+        });
+    } catch (e) {
+        console.log("Could not fetch API", e);
     }
+};
 
+function Load() {
     return (
         <button onClick={loadAPI} id="temp">Load JSON</button>
     );
