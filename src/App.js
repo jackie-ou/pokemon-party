f4d861cb19c605bf6748ba362bc474d23ea3c6bd
Safiriaa <101689115+Safiriaa@users.noreply.github.com>
2023-01-10 21:31:39 -0800

    search bar progress

--- src/App.js
+++ src/App.js
@@ -3,36 +3,61 @@ import Pokemon from './Pokemon.js';
 import { loadPokemonData } from './Load.js';
 import './App.css';
 import './Pokemon.js';
-import {uesEffect, useEffect, useState} from 'react';
+import {useEffect, useState} from 'react';
 
 function App() {
   const [pokemonAPIResponse, setPokemonAPIResponse] = useState(undefined);
+  const [userSearch, setUserSearch] = useState('');
   
   useEffect(() =>{
     loadPokemonData().then(response => setPokemonAPIResponse(response));
-    console.log(pokemonAPIResponse);
+    // console.log(pokemonAPIResponse);
   }, []);
 
+  useEffect(() =>{
+    const form = document.getElementById('userInput').value;
+    setUserSearch(form);
+    console.log(`User input: ${form}`);
+  }, []);
+
+  function getUserInput(){
+    // const userSearch = document.getElementById('userInput').value;
+    // console.log(`User input: ${userSearch}`);
+    // setUserSearch(userSearch);
+  }
+
+  let pokedex = [];
+  let searchResults = [];
+
   // For each pokemon, store name in array
-  // const pokedex = pokemonAPIResponse.map(data => data.name);
-  // console.log(pokedex);
+  if(pokemonAPIResponse) {
+    pokedex = pokemonAPIResponse.map(data => data.name);
+    console.log(`Pokedex: ${pokedex}`);
+    searchResults = pokedex.filter(pokemon => pokemon.includes(userSearch));
+    console.log(`Search results: ${searchResults}`);
+  }
 
   return (
-    <div className="container">
-      <section className="red-container">
-        <ul id="party">
-          <Pokemon></Pokemon>
-          <Pokemon></Pokemon>
-          <Pokemon></Pokemon>
-          <Pokemon></Pokemon>
-          <Pokemon></Pokemon>
-          <Pokemon></Pokemon>
-        </ul>
-      </section>
-      <section className="blue-container">
-        <img src={pokeball}></img>
-      </section>
-    </div>
+    <>
+      <form>
+        <input type='text' id='userInput'></input>
+      </form>
+      <div className="container">
+        <section className="red-container">
+          <ul id="party">
+            <Pokemon></Pokemon>
+            <Pokemon></Pokemon>
+            <Pokemon></Pokemon>
+            <Pokemon></Pokemon>
+            <Pokemon></Pokemon>
+            <Pokemon></Pokemon>
+          </ul>
+        </section>
+        <section className="blue-container">
+          <img src={pokeball}></img>
+        </section>
+      </div>
+    </>
   );
 }
 
--- src/Load.js
+++ src/Load.js
@@ -1,5 +1,6 @@
 async function loadPokemonData(){
-    const promise = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=2');
+    const promise = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=3');
+    // const promise = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=1154');
     const data = await promise.json();
     return data.results;
 }
