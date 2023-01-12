7ecdeaf034e37e5d1933b1b722851232c98ae72a
Zhongning Li <60045212+tomli380576@users.noreply.github.com>
2023-01-11 17:01:34 -0800

    feat: discussion code

--- src/App.js
+++ src/App.js
@@ -6,59 +6,62 @@ import "./Pokemon.js";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [pokemonAPIResponse, setPokemonAPIResponse] = useState(undefined);
-  const [query, setQuery] = useState("");
+    // const [pokemonAPIResponse, setPokemonAPIResponse] = useState(undefined);
+    const [allPokemons, setAllPokemons] = useState([]);
+    const [displayCount, setDisplayCount] = useState("max"); // number or 'max'
+    const [query, setQuery] = useState("");
 
-  useEffect(()=>{
-    loadPokemonData().then((response) => {
-      console.log(response);
-      setPokemonAPIResponse(response);
-    })
-  }, []);
+    useEffect(() => {
+        loadPokemonData().then(response => {
+            console.log(response);
+            // For each pokemon, store name in array
+            setPokemonAPIResponse(response.map(data => data.name));
+        });
+    }, []);
 
-
-
-  let pokedex = [];
-
-  // For each pokemon, store name in array
-  if (pokemonAPIResponse) {
-    pokedex = pokemonAPIResponse.map((data) => data.name);
-    console.log(`Pokedex: ${pokedex}`);
-  }
-
-  return (
-    <>
-      <div className="container">
-        <section className="red-container">
-          <ul id="party">
-            <Pokemon></Pokemon>
-            <Pokemon></Pokemon>
-            <Pokemon></Pokemon>
-            <Pokemon></Pokemon>
-            <Pokemon></Pokemon>
-            <Pokemon></Pokemon>
-          </ul>
-        </section>
-        <section className="blue-container">
-          <input
-            type="text"
-            placeholder="Search..."
-            onChange={(e) => setQuery(e.target.value)}
-          />
-          <ul className="search-results">
-            {pokedex.filter((pokemon) =>
-              pokemon.includes(query)).map((name, id) => (
-                <li key={id} className="search-results-item">
-                  {name}
-                </li>
-              ))
-            }
-          </ul>
-          <img src={pokeball}></img>
-        </section>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="container">
+                <section className="red-container">
+                    <ul id="party">
+                        <Pokemon></Pokemon>
+                        <Pokemon></Pokemon>
+                        <Pokemon></Pokemon>
+                        <Pokemon></Pokemon>
+                        <Pokemon></Pokemon>
+                        <Pokemon></Pokemon>
+                    </ul>
+                </section>
+                <section className="blue-container">
+                    <input
+                        type="text"
+                        placeholder="Search..."
+                        onChange={e => setQuery(e.target.value)}
+                    />
+                    <button
+                        onClick={() =>
+                            setDisplayCount(
+                                Math.min(displayCount + 10, allPokemons.length)
+                            )
+                        }
+                    >
+                        Show 10 More
+                    </button>
+                    <ul className="search-results">
+                        {allPokemons
+                            .filter(pokemon => pokemon.includes(query))
+                            .map((name, id) => (
+                                <li key={id} className="search-results-item">
+                                    {name}
+                                </li>
+                            ))
+                            .slice(0, displayCount)}
+                    </ul>
+                    <img src={pokeball}></img>
+                </section>
+            </div>
+        </>
+    );
 }
 
 export default App;
